477cff75b5cbe8862fe0bbfde9e62eb38bd3a099
Ronan O'Leary <roleary81@gmail.com>
2025-01-25 20:36:09 +0100

    add s1m1l4r errorbots

--- src/app/all-errorbots/[slug]/page.tsx
+++ src/app/all-errorbots/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 type tParams = Promise<{ slug: string[] }>;
 
@@ -11,6 +12,24 @@ export default async function Page(props: { params: tParams }) {
   const product = {
     name: `ErrorBot ${slug}`,
     image: `/assets/images/${slug}.webp`,
+  };
+
+  const products: Product[] = [
+    { id: 2, slug: "errorbot-2", name: "Miguel", price: 29.99, image: "/assets/images/errorbot-2.webp" },
+    { id: 3, slug: "errorbot-3", name: "Leonard", price: 39.99, image: "/assets/images/errorbot-3.webp" },
+    { id: 24, slug: "errorbot-24", name: "Solo", price: 249.99, image: "/assets/images/errorbot-24.webp" },
+  ];
+
+  const handleClick = (product) => {
+    if (document.startViewTransition) {
+      // Use the View Transition API if available
+      document.startViewTransition(() => {
+        router.push(`/all-errorbots/${product.slug}`);
+      });
+    } else {
+      // Fallback if View Transitions API isn't supported
+      router.push(`/all-errorbots/${product.slug}`);
+    }
   };
 
   return (
@@ -39,6 +58,38 @@ export default async function Page(props: { params: tParams }) {
             </Button>
           </div>
         </div>
+      </div>
+
+      <div className="w-full mx-auto py-8 px-8 max-sm:p-1">
+
+    
+      <h3 className="font-black text-2xl px-8 py-8">S1M1L4R ERRORBOTS</h3>
+      <div className="sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 flex overflow-x-auto flex-nowrap sm:flex-wrap pb-8">
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              className="flex flex-col h-full relative group border border-transparent hover:border-red-500 hover:border-4 transition-all min-w-[75%] sm:min-w-0"
+            >
+              <CardContent className="p-4 flex flex-col h-full">
+                <div className="relative w-full pb-[100%]">
+                  <a href="#" className="cursor-pointer">
+                    <Image
+                      src={product.image || "/placeholder.svg"}
+                      alt={product.name}
+                      fill
+                      className="object-cover rounded-md"
+                    />
+                  </a>
+                </div>
+                <div className="mt-4 flex-grow">
+                  <h3 className="text-lg font-semibold truncate">{product.name}</h3>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+
+
       </div>
     </main>
   );
